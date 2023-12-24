209acce01c28c09d6f19b849cac25cd58d4de762
Matgadson <gadson.ramalepe4@gmail.com>
2023-12-24 18:33:53 +0200

    smartupdatet

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,4 +1,5 @@
 from brownie import network, accounts, config, MockV3Aggregator
+
 # from web3 import Web3
 
 DECIMALS = 18
@@ -17,7 +18,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks>......")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, STARTING_PRICE, {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks deployed")
